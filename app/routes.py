18efc79e2f87f6d3f3c54a49be688b5425ad3a90
Lache Alexandra Florentina Georgiana <alexandralache0802@yahoo.com>
2025-06-22 12:00:39 +0300

    Remove redundant endpoint method check

--- app/routes.py
+++ app/routes.py
@@ -8,28 +8,27 @@ from app.task import TaskFactory
 @webserver.route('/api/get_results/<job_id>', methods=['GET'])
 def get_response(job_id):
     '''Get result for a job'''
-    if request.method == 'GET':
-        webserver.logger.info("Got request for job with id %s", job_id)
-        # Check if job_id is valid
-        id_num = int(job_id)
-        if id_num > webserver.job_counter:
-            return jsonify({"status": "error", "reason" : "Invalid job_id"})
+    webserver.logger.info("Got request for job with id %s", job_id)
 
-        if webserver.tasks_runner.get_task_status(id_num) == "done":
-            #retrieve the data from the file and return it
-            file_path = os.path.join("results", f"job_id_{id_num}.json")
-            with open(file_path, "r", encoding='UTF-8') as f:
-                result = json.load(f)
-            message = {"status": "done", "data" : result}
-        else:
-            message = {"status": "running"}
-        webserver.logger.info("Returned status for job %s", job_id)
-        return jsonify(message)
-    return jsonify({"error": "Method not allowed"}), 405
+    # Check if job_id is valid
+    id_num = int(job_id)
+    if id_num > webserver.job_counter:
+        return jsonify({"status": "error", "reason" : "Invalid job_id"})
+
+    if webserver.tasks_runner.get_task_status(id_num) == "done":
+        #retrieve the data from the file and return it
+        file_path = os.path.join("results", f"job_id_{id_num}.json")
+        with open(file_path, "r", encoding='UTF-8') as f:
+            result = json.load(f)
+        message = {"status": "done", "data" : result}
+    else:
+        message = {"status": "running"}
+    webserver.logger.info("Returned status for job %s", job_id)
+    return jsonify(message)
 
 
-def add_task_to_threadpool(task_name: str, data, has_state: bool = False):
-    '''Helper function for POST methods'''
+def submit_task_to_threadpool(task_name: str, data, has_state: bool = False):
+    '''Helper function for POST methods to submit tasks to the threadpool'''
     webserver.logger.info("Got request : %s", str(data))
 
     # Not adding tasks in the queue after graceful shutdown
@@ -60,91 +59,65 @@ def add_task_to_threadpool(task_name: str, data, has_state: bool = False):
 @webserver.route('/api/states_mean', methods=['POST'])
 def states_mean_request():
     '''States_mean request function'''
-    if request.method == 'POST':
-        data = request.json
-
-        return add_task_to_threadpool("states_mean", data)
+    data = request.json
+    return submit_task_to_threadpool("states_mean", data)
     return jsonify({"error": "Method not allowed"}), 405
 
 
 @webserver.route('/api/state_mean', methods=['POST'])
 def state_mean_request():
     '''State_mean request function'''
-    if request.method == 'POST':
-        data = request.json
-
-        return add_task_to_threadpool("state_mean", data, True)
-    return jsonify({"error": "Method not allowed"}), 405
+    data = request.json
+    return submit_task_to_threadpool("state_mean", data, True)
 
 
 @webserver.route('/api/best5', methods=['POST'])
 def best5_request():
     '''Best5 request function'''
-    if request.method == 'POST':
-        data = request.json
-
-        return add_task_to_threadpool("best5", data)
-    return jsonify({"error": "Method not allowed"}), 405
+    data = request.json
+    return submit_task_to_threadpool("best5", data)
 
 
 @webserver.route('/api/worst5', methods=['POST'])
 def worst5_request():
     '''Worst5 request function'''
-    if request.method == 'POST':
-        data = request.json
-
-        return add_task_to_threadpool("worst5", data)
-    return jsonify({"error": "Method not allowed"}), 405
+    data = request.json
+    return submit_task_to_threadpool("worst5", data)
 
 
 @webserver.route('/api/global_mean', methods=['POST'])
 def global_mean_request():
     '''Global_mean request function'''
-    if request.method == 'POST':
-        data = request.json
-
-        return add_task_to_threadpool("global_mean", data)
-    return jsonify({"error": "Method not allowed"}), 405
+    data = request.json
+    return submit_task_to_threadpool("global_mean", data)
 
 
 @webserver.route('/api/diff_from_mean', methods=['POST'])
 def diff_from_mean_request():
     '''Diff_from_mean request function'''
-    if request.method == 'POST':
-        data = request.json
-
-        return add_task_to_threadpool("diff_from_mean", data)
-    return jsonify({"error": "Method not allowed"}), 405
+    data = request.json
+    return submit_task_to_threadpool("diff_from_mean", data)
 
 
 @webserver.route('/api/state_diff_from_mean', methods=['POST'])
 def state_diff_from_mean_request():
     '''State_diff_from_mean request function'''
-    if request.method == 'POST':
-        data = request.json
-
-        return add_task_to_threadpool("state_diff_from_mean", data, True)
-    return jsonify({"error": "Method not allowed"}), 405
+    data = request.json
+    return submit_task_to_threadpool("state_diff_from_mean", data, True)
 
 
 @webserver.route('/api/mean_by_category', methods=['POST'])
 def mean_by_category_request():
     '''Mean_by_category request function'''
-    if request.method == 'POST':
-        data = request.json
-
-        return add_task_to_threadpool("mean_by_category", data)
-    return jsonify({"error": "Method not allowed"}), 405
+    data = request.json
+    return submit_task_to_threadpool("mean_by_category", data)
 
 
 @webserver.route('/api/state_mean_by_category', methods=['POST'])
 def state_mean_by_category_request():
     '''State_mean_by_category request function'''
-    if request.method == 'POST':
-        data = request.json
-
-        return add_task_to_threadpool("state_mean_by_category", data, True)
-    return jsonify({"error": "Method not allowed"}), 405
+    data = request.json
+    return submit_task_to_threadpool("state_mean_by_category", data, True)
 
 
 @webserver.route('/api/graceful_shutdown', methods=['GET'])
